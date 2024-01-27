f9a061fc89c50ed8ea9caf1ec9ecfa428c275b9f
Foxie Flakey <foxieflakey@gmail.com>
2024-01-27 11:24:46 +0700

    Reset UwU

--- sample/app/common/special.c
+++
@@ -1,6 +0,0 @@
-#include "generated/kconfig_config.h"
-
-const char* some_app_special() {
-  return "I'm special UwU";
-}
-
--- sample/extra_lib_shared/include/extra_lib_shared.h
+++
@@ -1,8 +0,0 @@
-#ifndef header_1702722664_9bbbb399_a751_4754_96c8_87c0e958ad56_extra_lib_h
-#define header_1702722664_9bbbb399_a751_4754_96c8_87c0e958ad56_extra_lib_h
-
-__attribute__((visibility("default")))
-extern int exported_shared_uwu(const char* msg);
-
-#endif
-
